927eba4a937a80976448919025eb03b15135c5fe
Daniel <danielinfomlg@gmail.com>
2023-11-15 11:53:53 +0100

    Porgreso hasta prueba crear finalizada y antes de realizar el ejercicio de DELETE

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 
 const express = require("express");
-
 const {leerTareas,crearTarea,editarTextoTarea,borrarTarea} = require("./db");
+const {json} = require("body-parser");
 
 const servidor = express();
+
+servidor.use(json());
 
 servidor.use("/pruebas-api",express.static("./pruebas_api"));
 
@@ -22,13 +24,33 @@ servidor.get("/tareas", async (peticion,respuesta,siguiente) => {
 
 });
 
+servidor.post("/nueva", async (peticion,respuesta,siguiente) => {
+    let {tarea} = peticion.body;
+
+    if(!tarea){
+        return siguiente(1);
+    }
+    let [error,id] = await crearTarea(tarea);
+
+    if(error){
+        return siguiente(2);
+    }
+
+    respuesta.json({ resultado : "ok", id});
+});
+
 servidor.use((error,peticion,respuesta,siguiente) => {
     // cualquier excepción que envíe el sistema (throw) será capturada por este middleware
     switch(error){
-        case 1: return respuesta.send("...error en la petición");
+        case 1: 
+            respuesta.status(400);
+            return respuesta.json({error : "error en la petición, faltan parametros"});
         case 2: 
             respuesta.status(500);
             return respuesta.json({ error : "error en el servidor"});
+        default:
+            respuesta.status(400);
+            return respuesta.json({error : "error en la petición, objeto JSON mal formado"});
     }
     
     /*console.log(error);
